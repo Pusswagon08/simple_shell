567247ca2464b60c3b0e4f98ff101069c8ae52d2
Pussywagon08 <rilwanabioye08@gmail.com>
2023-05-10 14:59:26 -0700

    created task 0

---
+++ task-0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints out name's passed to it
+ *
+ * Return: return count
+ *
+ */
+
+int main(void)
+{
+	int count = 2;
+
+	printf("This simple_shell repo was created by Pussywagon08 and Sarifta\n");
+	return (count);
+}
